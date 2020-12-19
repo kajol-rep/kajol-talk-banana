3a8b50b6d41a862e591d5fb352bd7aa59c467d55
kajol-rep <75063748+kajol-rep@users.noreply.github.com>
2020-12-19 19:49:24 +0530

    input output processing done

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
-var username = prompt("hello enter your name: ")
-alert("welcome "+username)
+var btnTranslate=document.querySelector("#btn-translate")
+var txtInput=document.querySelector("#txt-input")
+var outPut=document.querySelector("#output")
+
+console.log(txtInput)
+
+function clickHandler(){
+    console.log("clicked")
+    console.log("input =>", txtInput.value)
+    outPut.innerText = "kadunaggbaba " + txtInput.value
+}
+
+btnTranslate.addEventListener("click", clickHandler)
